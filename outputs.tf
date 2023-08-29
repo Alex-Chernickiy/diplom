31457107a2de5151c35d06bdf88a9fe181ffc65e
Alex Chernitsky <4eri7s@mail.ru>
2023-08-29 12:52:13 +0300

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "cluster_id" {
-  description = "EKS cluster ID."
-  value       = aws_eks_cluster.my-cluster.id
-}
-
 output "cluster_name" {
   description = "EKS cluster name."
   value       = aws_eks_cluster.my-cluster.name
@@ -13,30 +8,25 @@ output "ecr_name" {
   value       = aws_ecr_repository.ecr.name
 }
 
-output "ecr_id" {
-  description = "ECR repository ID."
-  value       = aws_ecr_repository.ecr.id
-}
-
 output "nat_eip1_public_ip1" {
   description = "Contains the public IP address"
   value       = aws_eip.nat_eip1.public_ip
 }
 
-# output "nat_eip2_private_ip1" {
-#   description = "Contains the private IP address"
-#   value       = aws_eip.nat_eip1.private_ip
-# }
+output "nat_eip2_private_ip1" {
+  description = "Contains the private IP address"
+  value       = aws_eip.nat_eip1.private_ip
+}
 
 output "nat_eip1_public_ip2" {
   description = "Contains the public IP address"
   value       = aws_eip.nat_eip2.public_ip
 }
 
-# output "nat_eip2_private_ip2" {
-#   description = "Contains the private IP address"
-#   value       = aws_eip.nat_eip2.private_ip
-# }
+output "nat_eip2_private_ip2" {
+  description = "Contains the private IP address"
+  value       = aws_eip.nat_eip2.private_ip
+}
 
 output "vpc_id" {
   description = "ID of the vpc"
@@ -56,11 +46,6 @@ output "eks_node_group_id" {
 output "eks_node_group_status" {
   description = "Status of the node group"
   value       = aws_eks_node_group.my_node_group.status
-}
-
-output "eks_node_group_recources" {
-  description = "Recources of the node group"
-  value       = aws_eks_node_group.my_node_group.resources
 }
 
 output "eks_node_group_subnet_ids" {
